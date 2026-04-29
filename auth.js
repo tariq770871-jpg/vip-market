c249a52d1f247dcb9b21851e365223fab03b8910
tariq770871-jpg <tariq770871@gmail.com>
2026-04-29 20:41:09 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -4,3 +4,7 @@ const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 const SUPABASE_URL = "https://hrbhuvirarvwwbbjbwzl.supabase.co";
 const SUPABASE_KEY = "ضع هنا الـ Anon Key الذي نسخته من Supabase";
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const SUPABASE_URL = "https://hrbhuvirarvwwbbjbwzl.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhyYmh1dmlyYXJ2d3diYmpid3psIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczMzM1MjUsImV4cCI6MjA5MjkwOTUyNX0._6lEUIOMjNgz0SqsQScDUhocSTGvcY9nAB136HKTdL0";
+
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
