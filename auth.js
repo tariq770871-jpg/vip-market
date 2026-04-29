f186880c6bb827f2bb5e2690b5a4db0b9143a01f
tariq770871-jpg <tariq770871@gmail.com>
2026-04-29 20:36:10 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,3 +1,6 @@
 const SUPABASE_URL = "https://YOUR_PROJECT_URL.supabase.co";
 const SUPABASE_KEY = "YOUR_ANON_KEY";
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const SUPABASE_URL = "https://hrbhuvirarvwwbbjbwzl.supabase.co";
+const SUPABASE_KEY = "ضع هنا الـ Anon Key الذي نسخته من Supabase";
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
